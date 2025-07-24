d6d46363ece8224e10c9965b9f4261dfb55d2931
hariom224208 <hariom723707@gmail.com>
2025-07-24 14:13:09 +0530

    Add files via upload

---
+++ src/bank/mamagement/system/Con.java
@@ -0,0 +1,19 @@
+package bank.mamagement.system;
+
+import java.sql.*;
+
+public class Con {
+    Connection connection;
+    Statement statement;
+
+    public Con(){
+        try{
+            connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","root");
+            statement=connection.createStatement();
+
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
